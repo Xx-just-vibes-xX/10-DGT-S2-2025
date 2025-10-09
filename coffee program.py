ed67ebcb0dd53bc9a0a78dfcba014f5e65d3f70e
Xx-just-vibes-xX <timea.croucamp@kingsway.school.nz>
2025-10-10 10:37:42 +1300

    Coffee program Version 1
    Asked the user whether they like coffee
    Recorded the answer
    Gave a response

---
+++ coffee program.py
@@ -0,0 +1,18 @@
+# Creating an input system for our coffee program
+# Timea Croucamp
+# Date: 10/10/2025
+
+# Version 1
+# TODO: Ask the user if they like coffee
+#       Record their answer
+#       Give a response back to the user
+
+# Ask the user whether they like coffee or not
+like_coffee = input("Do you like coffee?")
+print(f'Your answer was "{like_coffee}".')
+
+if like_coffee == "yes" or "Yes" or "y" or "Y" :
+    print("That is great! I like coffee too.")
+
+else:
+    print("You are missing out! Why not give it a try?")
