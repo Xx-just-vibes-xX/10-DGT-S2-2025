c8334c456c9782db19ef532663cf6ab33cbccd2b
Xx-just-vibes-xX <timea.croucamp@kingsway.school.nz>
2025-10-29 15:10:17 +1300

    Number checker for area perimeter program v 1.0

---
+++ number checker.py
@@ -0,0 +1,16 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+
+def num_check(question):...
+
+
+# Main Routine goes here
+for item in range(0, 2):
+    width = num_check("Width: ")
+    print(width)
+
+print ()
+
+for item in range(0, 2):
+    height = num_check("Height: ")
+    print(height)
